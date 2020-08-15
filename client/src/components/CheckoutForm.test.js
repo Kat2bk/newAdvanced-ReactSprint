19a7096c8372e51201f313ceac505618445ba0a0
unknown <Katherine2bk@gmail.com>
2020-08-15 10:09:52 -0700

    second test has passed for form details

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -10,4 +10,36 @@ test("form header renders", () => {
     expect(header).toBeInTheDocument();
 });
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+    const {getByLabelText, getByText, getByTestId} = render(<CheckoutForm />);
+
+    const firstNameInput = getByLabelText(/first name/i);
+    const lastNameInput = getByLabelText(/last name/i);
+    const addressInput = getByLabelText(/address/i);
+    const cityInput = getByLabelText(/city/i);
+    const stateInput = getByLabelText(/state/i);
+    const zipInput = getByLabelText(/zip/i);
+
+    fireEvent.change(firstNameInput, {target: {value: "Katherine"}});
+    fireEvent.change(lastNameInput, {target: {value: "Kumar"}});
+    fireEvent.change(addressInput, {target: {value: "Jefferson"}});
+    fireEvent.change(cityInput, {target: {value: "LC"}});
+    fireEvent.change(stateInput, {target: {value: "Louisiana"}});
+    fireEvent.change(zipInput, {target: {value: "706"}});
+
+    expect(firstNameInput.value).toBe("Katherine");
+    expect(lastNameInput.value).toBe("Kumar");
+    expect(addressInput.value).toBe("Jefferson");
+    expect(cityInput.value).toBe("LC");
+    expect(stateInput.value).toBe("Louisiana");
+    expect(zipInput.value).toBe("706");
+
+    const submit = getByText("Checkout");
+    fireEvent.click(submit);
+
+    const getName = getByText(/katherine/i);
+    expect(getName).toBeInTheDocument();
+
+    const success = getByTestId(/successMessage/i);
+    expect(success).toBeInTheDocument();
+});
