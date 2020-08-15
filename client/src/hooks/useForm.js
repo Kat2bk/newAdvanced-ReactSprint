fccdbe5c73f918efbe634017254c42a6f8875616
unknown <Katherine2bk@gmail.com>
2020-08-15 09:50:59 -0700

    set up hook for handleChanges, imported useLocalStorage hook, adding to form now

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,12 @@
 // write your custom hook here to control your checkout form
+import {useLocalStorage} from "./useLocalStorage";
+
+export const useForm = (key, initialValues) => {
+    const [values, setValues] = useLocalStorage(key, initialValues);
+
+    const handleChanges = (event) => {
+        setValues({...values, [event.target.name]: event.target.value});
+    }
+
+    return [values, handleChanges];
+}
